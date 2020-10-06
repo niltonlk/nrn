46e2f2d931a05a73dca16c9198b383cc5e86d4a8
Alexandru Savulescu <alexandru.savulescu@epfl.ch>
2020-10-06 14:37:06 +0200

    modern cpp : src/nrnjava

--- src/nrnjava/njvm.h
+++ src/nrnjava/njvm.h
@@ -4,16 +4,16 @@
 #define jnisave JNIEnv* jesave = nrnjava_env; nrnjava_env = env;
 #define jnirestore nrnjava_env = jesave;
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
+//#if defined(__cplusplus)
+//extern "C" {
+//#endif
 
 extern JNIEnv* nrnjava_env;
 extern JNIEnv* nrnjava_root_env;
 
-#if defined(__cplusplus)
-}
-#endif
+//#if defined(__cplusplus)
+//}
+//#endif
 
 
 #endif
