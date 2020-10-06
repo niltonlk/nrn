2b52ca163fb334672bcdea13a9a2b05dda5a532c
Alexandru Savulescu <alexandru.savulescu@epfl.ch>
2020-10-06 14:44:28 +0200

    modern cpp : src/oc

--- src/oc/nrnmpi.h
+++ src/oc/nrnmpi.h
@@ -21,16 +21,16 @@ typedef struct {
 	        
 #if NRNMPI
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
+//#if defined(__cplusplus)
+//extern "C" {
+//#
 
 extern int nrnmpi_use; /* NEURON does MPI init and terminate?*/
 extern int nrn_cannot_use_threads_and_mpi; /* 0 if required <= provided from MPI_Init_thread */
 
-#if defined(__cplusplus)
-}
-#endif /*c++*/
+//#if defined(__cplusplus)
+//}
+//#endif /*c++*/
 
 #include "nrnmpidec.h"
 
--- src/oc/ocmisc.h
+++ src/oc/ocmisc.h
@@ -1,11 +1,20 @@
 #ifndef ocmisc_h
 #define ocmisc_h
 
+//#if defined(__cplusplus)
+//extern "C" {
+//#
+
 extern long hoc_nstack;
 extern long hoc_nframe;
 extern int hoc_errno_count;
 extern int hoc_pipeflag;
 extern int hoc_in_yyparse;
 extern int hoc_intset;
+
+//#if defined(__cplusplus)
+//}
+//#endif
+
 
 #endif
